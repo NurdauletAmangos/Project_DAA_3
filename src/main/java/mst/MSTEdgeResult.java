4fbe39c5c14b02beba139e28f53663e21b83e670
Nurdauletamangos <nurdauletamangosvps@gmail.com>
2025-10-26 23:16:52 +0500

    Add result data structures and JSON writer for output

---
+++ src/main/java/mst/MSTEdgeResult.java
@@ -0,0 +1,13 @@
+package mst;
+
+public class MSTEdgeResult {
+    public String from;
+    public String to;
+    public int weight;
+
+    public MSTEdgeResult(String from, String to, int weight) {
+        this.from = from;
+        this.to = to;
+        this.weight = weight;
+    }
+}
